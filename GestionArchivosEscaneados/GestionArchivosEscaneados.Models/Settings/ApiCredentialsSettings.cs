c2e3eca2cc5c8d2683b2e5e5479b950969617894
alejoortiz18 <45045371+alejoortiz18@users.noreply.github.com>
2026-06-22 08:36:51 -0500

    subir cambis para el OpenIA
    cambios para opne ai settings

--- GestionArchivosEscaneados/GestionArchivosEscaneados.Models/Settings/ApiCredentialsSettings.cs
+++ GestionArchivosEscaneados/GestionArchivosEscaneados.Models/Settings/ApiCredentialsSettings.cs
@@ -5,4 +5,6 @@ public class ApiCredentialsSettings
     public string SoporteApiKey { get; set; } = string.Empty;
 
     public string SoporteFisicoToken { get; set; } = string.Empty;
+
+    public string IdUsuario { get; set; } = "system";
 }
